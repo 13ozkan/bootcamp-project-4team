e2e2e4186803b1a437cd53e0e80d75023a1a81e9
Ozkan Ozturk <73650840+13ozkan@users.noreply.github.com>
2022-05-09 15:31:11 +0300

    added zombie leg pack

---
+++ 4Project/Assets/Models/Zombie Leg/Scripts/PlayANim.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayANim : MonoBehaviour
+{
+
+    public Animator pla1;
+    public string anim1;
+    public string anim2;
+    public string anim3;
+    public string anim4;
+    public string anim5;
+    public string anim6;
+    public string anim7;
+    public string anim8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    public void anim1Press()
+    {
+        pla1.Play(anim1);
+       
+    }
+    public void anim2Press()
+    {
+        pla1.Play(anim2);
+       
+    }
+    public void anim3Press()
+    {
+        pla1.Play(anim3);
+        
+    }
+    public void anim4Press()
+    {
+        pla1.Play(anim4);
+       
+    }
+    public void anim5Press()
+    {
+        pla1.Play(anim5);
+
+    }
+    public void anim6Press()
+    {
+        pla1.Play(anim6);
+
+    }
+    public void anim7Press()
+    {
+        pla1.Play(anim7);
+
+    }
+    public void anim8Press()
+    {
+        pla1.Play(anim8);
+
+    }
+
+}
