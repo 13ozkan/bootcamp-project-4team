a82d01be051daeac4c35bd615b22ff70c13ef1f3
Utku <utku_gogen@hotmail.com>
2022-06-06 22:47:10 +0300

    Beyin Boss Savaş v4
    Kazanma animasyonu değiştirmeler.

--- 4Project/Assets/Scripts/Finish.cs
+++ 4Project/Assets/Scripts/Finish.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Finish : MonoBehaviour
 {
     public AnimatorOverrideController kazanmaAnimationOverride;
+    public AnimatorOverrideController danceAnimationOverride;
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Collected" || other.gameObject.tag == "Player")
         {
-            //kazanma animasyonuna çevir
-            other.gameObject.GetComponent<Animator>().runtimeAnimatorController = kazanmaAnimationOverride;
+    
 
             if(other.gameObject.GetComponent<Player>() != null)
             {
+                //kazanma animasyonuna çevir
+                other.gameObject.GetComponent<Animator>().runtimeAnimatorController = danceAnimationOverride;
                 other.gameObject.GetComponent<Player>().hiz = 0;
+                other.gameObject.transform.DORotate(new Vector3(0, 180, 0), 1f);
+            }
+            else
+            {
+                other.gameObject.GetComponent<Animator>().runtimeAnimatorController = kazanmaAnimationOverride;
+                other.gameObject.GetComponent<Animator>().SetFloat("walkMultiplier", 1f);
             }
             
         }
